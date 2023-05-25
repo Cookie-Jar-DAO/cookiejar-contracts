0124f101629fe1616d9297d08518308fe9e8a2d7
dekanbro <dekanbrown@odyssy.io>
2023-05-25 11:19:17 -0600

    update tokenuri for nft

--- src/ERC6551/nft/CookieNFT.sol
+++ src/ERC6551/nft/CookieNFT.sol
@@ -13,6 +13,7 @@ import {CookieJarCore} from "src/core/CookieJarCore.sol";
 import {AccountERC6551} from "src/ERC6551/erc6551/ERC6551Module.sol";
 import {IAccount} from "src/interfaces/IERC6551.sol";
 import {MinimalReceiver} from "src/lib/MinimalReceiver.sol";
+import {Base64} from "src/lib/Base64.sol";
 
 contract CookieNFT is ERC721 {
     using Counters for Counters.Counter;
@@ -35,7 +36,7 @@ contract CookieNFT is ERC721 {
         address _erc6551Imp,
         address _cookieJarSummoner,
         address _cookieJarImp
-    ) ERC721("CookieJar", "COOKIE") {
+    ) ERC721("CookieJar Number 1", "COOKIE1") {
         erc6551Reg = _erc6551Reg;
         erc6551Imp = _erc6551Imp;
         cookieJarSummoner = _cookieJarSummoner;
@@ -70,12 +71,68 @@ contract CookieNFT is ERC721 {
                 cookieToken,
                 allowList
             ),
-            '{"type":"list6551", "title":"Cookie NFT", "title":"Cookie Util NFT"}'
+            '{"type":"CookieNFT1", "title":"Cookie NFT Gen 1", "description":"Gen1 Cookie"}'
         );
 
         CookieJarCore(cookieJar).transferOwnership(account);
         AccountERC6551(payable(account)).setExecutorInit(cookieJar);
 
         emit AccountCreated(account, cookieJar, tokenId);
+    }
+
+    /**  Constructs the tokenURI, separated out from the public function as its a big function.
+     * Generates the json data URI 
+     * param: _tokenId the tokenId
+     */
+    function _constructTokenURI(uint256 _tokenId)
+        internal
+        pure
+        returns (string memory)
+    {
+        string memory _nftName = string(abi.encodePacked("CookieNFT1"));
+        string memory _image = string(abi.encodePacked(
+            "https://ipfs.io/ipfs/QmWn8CP5AnqmPU2zKWZesk6EFhzk5zj72mdDQEaTPmwezF/",
+            Strings.toString(_tokenId)));
+        string memory _animation = string(abi.encodePacked(
+            "QmRjmYtyqetZgvmijsP1tKDD5dJG61vYFxThKBrUYXNZ4k?seed=",
+            Strings.toString(_tokenId)));
+        
+
+        return
+            string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name":"',
+                                _nftName,
+                                '", "image":"',
+                                _image,
+                                '", "animation_url":"',
+                                _animation,
+                                // Todo something clever
+                                '", "description": "Cookie Jar NFT", "attributes": [{"trait_type": "base", "value": "cookie"}]}'
+                            )
+                        )
+                    )
+                )
+            );
+    }
+
+    /* Returns the json data associated with this token ID
+     * param _tokenId the token ID
+     */
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(_tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+        return string(_constructTokenURI(_tokenId));
     }
 }
