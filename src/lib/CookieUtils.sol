f10d3d7f5d50091772892b4dfed63be8922ca5c7
bitbeckers <code@bitbeckers.com>
2023-06-17 13:16:13 +0200

    feat(uid): human readable uids

--- src/lib/CookieUtils.sol
+++ src/lib/CookieUtils.sol
@@ -3,28 +3,22 @@ pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
-import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
 
 library CookieUtils {
-   
     function getCookieJarUid(address cookieJarAddr) internal view returns (string memory) {
         return string(abi.encodePacked(Strings.toHexString(uint160(cookieJarAddr)), Strings.toHexString(block.chainid)));
     }
 
     function getCookieUid(string memory cookieJarUid) internal view returns (string memory) {
-        return string(abi.encodePacked(keccak256(abi.encodePacked(cookieJarUid, msg.sender, block.timestamp))));
-    }
-
-    /**
-     * @notice Converts a bytes32 value to a string.
-     * @dev This is a helper function that is used to convert bytes32 values to strings, for example, to convert hashed
-     * values
-     * to their string representation.
-     * @param _b The bytes32 value to convert.
-     * @return The string representation of the given bytes32 value.
-     */
-
-    function bytes32ToString(bytes32 _b) private pure returns (string memory) {
-        return string(abi.encodePacked(_b));
+        return Strings.toHexString(
+            uint256(
+                keccak256(
+                    abi.encodePacked(
+                        cookieJarUid, Strings.toHexString(msg.sender), Strings.toHexString(block.timestamp)
+                    )
+                )
+            )
+        );
     }
 }
